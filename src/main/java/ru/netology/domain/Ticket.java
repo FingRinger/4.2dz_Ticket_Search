62250f0fbf3eac30979772f3ade8cd36814e984f
FingRinger <ard21study@gmail.com>
2021-09-30 11:37:18 +0300

    Data-class Ticket with fields is created

---
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Ticket {
+    private int id;
+    private int price;
+    private int departureAirport;
+    private int arrivalAirport;
+    private int travelTime;
+
+}
